4c2a3af807d328eb650dd2e18e79e76aabed4662
nurlatif15 <163420393+nurlatif15@users.noreply.github.com>
2025-03-27 20:16:02 +0700

    Update cont.js

--- UndanganCont/assetcont/js/cont.js
+++ UndanganCont/assetcont/js/cont.js
@@ -16,3 +16,47 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
+
+// Reservasi
+document.getElementById("reservationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let nama = document.getElementById("nama").value;
+    let kehadiran = document.getElementById("kehadiran").value;
+    let pesan = document.getElementById("pesan").value;
+
+    if (nama.trim() === "") {
+        alert("Nama harus diisi!");
+        return;
+    }
+
+    let data = {
+        nama: nama,
+        kehadiran: kehadiran,
+        pesan: pesan
+    };
+
+    fetch("https://script.google.com/macros/s/AKfycbx3wzjgtMftrJMwyENdMm8h1Noa6GTxE_8zlK-OicZwR9kUP81P4crp90zfaQlfDWEO/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(() => {
+        document.getElementById("status").style.display = "block";
+        document.getElementById("reservationForm").reset();
+        setTimeout(() => {
+            document.getElementById("status").style.display = "none";
+        }, 3000);
+    }).catch(error => console.error("Error:", error));
+});
+
+
+
+// salin no rek 
+function salinRekening() {
+    var nomor = document.getElementById("nomorRekening").innerText;
+    navigator.clipboard.writeText(nomor);
+    alert("Nomor rekening berhasil disalin!");
+}
