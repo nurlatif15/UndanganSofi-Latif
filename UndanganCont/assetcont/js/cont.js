ba4282823e946778860a5319df543524ae289ab8
nurlatif15 <163420393+nurlatif15@users.noreply.github.com>
2025-04-01 17:45:09 +0700

    Update cont.js

--- UndanganCont/assetcont/js/cont.js
+++ UndanganCont/assetcont/js/cont.js
@@ -74,3 +74,12 @@ function copyAccount() {
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
 });
+
+        document.addEventListener("contextmenu", function(e) {
+            e.preventDefault();
+        });
+        document.addEventListener("keydown", function(e) {
+            if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I") || (e.ctrlKey && e.key === "U")) {
+                e.preventDefault();
+            }
+        });
